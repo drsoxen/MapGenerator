86a53ca185faebdfe7ab7882f6db1381721c10d5
Chris Ziraldo <chris.ziraldo@youi.tv>
2020-01-03 15:00:54 -0500

    Adding new map
    Removing

--- main.cpp
+++ main.cpp
@@ -36,45 +36,36 @@ void render() {
         std::string temp = "";
         getline(inFile,temp);
         latitude = strtof((temp).c_str(),0);
-        //std::cout << "latitude: " << latitude << std::endl;
         getline(inFile,temp);
         longitude = strtof((temp).c_str(),0);
-        //std::cout << "longitude: " << longitude << std::endl;
 
         int x =  (mapWidth/360.0) * (180 + longitude);
         int y =  (mapHeight/180.0) * (90 - latitude);
-
-        //std::cout << "x: " << x << " y: " << y << std::endl;
-        //std::cout << std::endl;
+        std::cout << x << y << std::endl;
 
         framebuffer[x+y*mapWidth] = Vec3f(3,57,82);
     }
     
-//    std::ifstream inFile2("airports.txt");
-//     
-//    for (int counter = 0; !inFile2.eof(); counter++)
-//    {
-//        float latitude    = 0; 
-//        float longitude   = 0;
-//
-//        std::string temp = "";
-//        getline(inFile2,temp);
-//        latitude = strtof((temp).c_str(),0);
-//        //std::cout << "latitude: " << latitude << std::endl;
-//        getline(inFile2,temp);
-//        longitude = strtof((temp).c_str(),0);
-//        //std::cout << "longitude: " << longitude << std::endl;
-//
-//        int x =  (mapWidth/360.0) * (180 + longitude);
-//        int y =  (mapHeight/180.0) * (90 - latitude);
-//
-//        //std::cout << "x: " << x << " y: " << y << std::endl;
-//        //std::cout << std::endl;
-//
-//        framebuffer[x+y*mapWidth] = Vec3f(255,0,0);
-//    }
+    std::ifstream inFile2("airports.txt");
+     
+    for (int counter = 0; !inFile2.eof(); counter++)
+    {
+        float latitude    = 0; 
+        float longitude   = 0;
 
-    std::ofstream ofs; // save the framebuffer to file
+        std::string temp = "";
+        getline(inFile2,temp);
+        latitude = strtof((temp).c_str(),0);
+        getline(inFile2,temp);
+        longitude = strtof((temp).c_str(),0);
+
+        int x =  (mapWidth/360.0) * (180 + longitude);
+        int y =  (mapHeight/180.0) * (90 - latitude);
+
+        //framebuffer[x+y*mapWidth] = Vec3f(255,0,0);
+    }
+
+    std::ofstream ofs;
     ofs.open("./out.ppm");
     ofs << "P6\n" << mapWidth << " " << mapHeight << "\n255\n";
     for (size_t i = 0; i < mapHeight*mapWidth; ++i) {
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ -std=c++14 *.cpp && ./a.out &&  open out.ppm; rm -r a.out
+g++ -std=c++14 *.cpp && ./a.out 
